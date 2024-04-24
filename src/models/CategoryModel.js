7d6314cacdd1a20e3f5eedebcbe3cd3efe96a052
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-24 15:13:05 +0600

    CategoryModel

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose');
 
 const DataSchema = mongoose.Schema({
-        categoryName:{type:String,unique:true},
-        categoryImg:{type:String,unique:true},
+        categoryName:{type:String,unique:true,required:true},
+        categoryImg:{type:String,required:true},
     },
     {timestamps: true,versionKey:false}
 )
