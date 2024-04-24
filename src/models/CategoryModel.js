387cbb2c3713bba867aa5938fcd3dc8af908dbc3
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-24 14:48:50 +0600

    CategoryModel Create

---
+++ src/models/CategoryModel.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+const DataSchema = mongoose.Schema({
+        categoryName:{type:String,unique:true},
+        categoryImg:{type:String,unique:true},
+    },
+    {timestamps: true,versionKey:false}
+)
+
+const CategoryModel=mongoose.model('categories',DataSchema)
+
+module.exports = CategoryModel;
