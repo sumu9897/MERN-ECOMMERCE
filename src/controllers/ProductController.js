c7e578091d015ed62f0d9a76821775730ca3958c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-25 10:05:53 +0600

    Product Controller

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,40 @@
+
+
+exports.BrandList=async (req, res)=>{
+
+}
+
+exports.CategoryList=async (req, res)=>{
+
+}
+
+exports.SliderList=async (req, res)=>{
+
+}
+
+exports.ListByBrand=async (req, res)=>{
+
+}
+
+exports.ListByCategory=async (req, res)=>{
+
+}
+
+exports.ListBySmilier=async (req, res)=>{
+
+}
+
+exports.ListByKeyword=async (req, res)=>{
+
+}
+
+exports.ProductReviewList=async (req, res)=>{
+
+}
+
+exports.CreateProductReview=async (req, res)=>{
+
+}
+
+
+
